43ee2ffd4e3b2eb0919893af6471be0b45febc35
Alexander Stone <enderu@gmail.com>
2019-12-05 13:01:59 -0600

    Day 3 Part 2. It's slow :\

--- day03/day03.py
+++ day03/day03.py
@@ -9,8 +9,8 @@ with open("03.in", "r+") as f:
   wire2 = puzzle_input[1].split(",")
 
 if test:
-  # wire1 = "R8,U5,L5,D3".split(",")
-  # wire2 = "U7,R6,D4,L4".split(",")
+  wire1 = "R8,U5,L5,D3".split(",")
+  wire2 = "U7,R6,D4,L4".split(",")
   wire1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72".split(",")
   wire2 = "U62,R66,U55,R34,D71,R55,D58,R83".split(",")
 
@@ -28,9 +28,9 @@ def plot_wire_path(ins):
 
   for i in ins:
     direction = trans[i[0]]
-    l = int(i[1:])
+    steps = int(i[1:])
 
-    for _ in range(0,l):
+    for _ in range(0, steps):
       last = tuple(map(operator.add, last, direction))
       points.add(last)
   
@@ -41,16 +41,59 @@ def find_overlaps(path1, path2):
   return path1 & path2
 
 
-def find_closest_overlap_md(overlaps):
+def find_closest_overlap(overlaps):
   ordered = sorted(overlaps, key=lambda x: abs(x[0]) + abs(x[1]))
-  return abs(ordered[0][0]) + abs(ordered[0][1])
+  return ordered[0]
 
 
 start_time = timeit.default_timer()
 
 overlaps = find_overlaps(plot_wire_path(wire1), plot_wire_path(wire2))
-closest_md = find_closest_overlap_md(overlaps)
+closest = find_closest_overlap(overlaps)
+closest_md = abs(closest[0]) + abs(closest[1])
 
 print(f"Manhattan Distance to closest overlap: {closest_md}")
 print(f"Completed in {round(timeit.default_timer()-start_time, 4)} seconds.")
 
+
+# Part Two
+def count_steps(ins, point):
+  trans = {
+    "R": (1,0),
+    "L": (-1,0),
+    "U": (0,1),
+    "D": (0,-1)
+    }
+
+  last = (0,0)
+  count = 0
+
+  for i in ins:
+    direction = trans[i[0]]
+    steps = int(i[1:])
+
+    for _ in range(0, steps):
+      last = tuple(map(operator.add, last, direction))
+      count += 1
+
+      if last == point:
+        break
+    else:
+      continue
+    break
+  
+  return count
+
+
+def find_lowest_step_count(ins, overlaps):
+  step_counts = [[count_steps(ins[0], overlap) + count_steps(ins[1], overlap) , overlap] for overlap in overlaps]
+  step_counts = sorted(step_counts, key=lambda x: x[0])
+
+  return step_counts[0]
+
+
+start_time = timeit.default_timer()
+fewest_steps = find_lowest_step_count([wire1, wire2], overlaps)[0]
+
+print(f"Fewest combined steps: {fewest_steps}")
+print(f"Completed in {round(timeit.default_timer()-start_time, 4)} seconds.")
